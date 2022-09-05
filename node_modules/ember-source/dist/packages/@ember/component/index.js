0aca14e6850e64fa00c69abadb515c45c0333d76
srinath <sri@mail.com>
2022-09-05 10:52:38 +0530

    after some ignores

--- node_modules/ember-source/dist/packages/@ember/component/index.js
+++
@@ -1,3 +0,0 @@
-export { setComponentTemplate, getComponentTemplate } from '@glimmer/manager';
-export { Component as default, Input } from '@ember/-internals/glimmer';
-export { componentCapabilities as capabilities, setComponentManager } from '@ember/-internals/glimmer';
