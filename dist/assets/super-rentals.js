b1621cf1c62401a8356bde63743a44763cc82453
srinath <sri@mail.com>
2022-09-05 06:52:31 +0530

    master commit

--- dist/assets/super-rentals.js
+++ dist/assets/super-rentals.js
@@ -915,7 +915,7 @@ catch(err) {
 
 ;
           if (!runningTests) {
-            require("super-rentals/app")["default"].create({"name":"super-rentals","version":"0.0.0+6bcc9203"});
+            require("super-rentals/app")["default"].create({"name":"super-rentals","version":"0.0.0+b6192a58"});
           }
         
 //# sourceMappingURL=super-rentals.map
